547f2c5b8dc74a0eca70e1a6679cd7bfd1a632d7
jiwoo kang <85946408+ji0u0@users.noreply.github.com>
2024-06-28 19:07:17 +0900

    add binary

--- Assets/Scripts/Phonebook.cs
+++ Assets/Scripts/Phonebook.cs
@@ -28,16 +28,19 @@ public class Phonebook : MonoBehaviour
     [SerializeField] public Button deleteButton;
     [SerializeField] public Text deleteResultText;
 
+    
 
     // Start is called before the first frame update
     void Start()
     {
-        contacts.Add(new Contact("jiwoo", "01092139670"));
-        contacts.Add(new Contact("sumin", "01020182373"));
-        contacts.Add(new Contact("ksi", "01092289010"));
-        contacts.Add(new Contact("mkr", "01064772374"));
-        contacts.Add(new Contact("kjw", "01088309670"));
+        // Add Data
+        contacts.Add(new Contact("kangjiwoo", "01092139670"));
+        contacts.Add(new Contact("kangsumin", "01020182373"));
+        contacts.Add(new Contact("kangsungil", "01092289010"));
+        contacts.Add(new Contact("moonkyungran", "01064772374"));
+        contacts.Add(new Contact("kangjaewook", "01088309670"));
 
+        // Add Listen to UI
         dropdown.onValueChanged.AddListener(delegate {
             DropdownValueChanged(dropdown);
         });
@@ -46,6 +49,7 @@ public class Phonebook : MonoBehaviour
         addButton.onClick.AddListener(AddContact);
         deleteButton.onClick.AddListener(DeleteContact);
 
+        // Init Setting
         DropdownValueChanged(dropdown);
     }
 
@@ -56,42 +60,70 @@ public class Phonebook : MonoBehaviour
 
     public void DropdownValueChanged(Dropdown dropdown)
     {
-        switch (dropdown.value)
-        {
-            case 0:
-                searchSession.SetActive(true);
-                addSession.SetActive(false);
-                deleteSession.SetActive(false);
-                break;
-            case 1:
-                searchSession.SetActive(false);
-                addSession.SetActive(true);
-                deleteSession.SetActive(false);
-                break;
-            case 2:
-                searchSession.SetActive(false);
-                addSession.SetActive(false);
-                deleteSession.SetActive(true);
-                break;
-            default:
-                break;
-        }
+        searchSession.SetActive(dropdown.value == 0);
+        addSession.SetActive(dropdown.value == 1);
+        deleteSession.SetActive(dropdown.value == 2);
     }
 
     public void SearchContact()
     {
-        string searchQuery = searchInput.text.Trim();
-        searchResultText.text = "";
+        string searchQuery = searchInput.text.Trim().ToLower();
+        // searchResultText.text = "";
 
+        // 검색어가 비었다면 전체 출력
+        if (string.IsNullOrEmpty(searchQuery))
+        {
+            searchResultText.text = string.Join("\n", contacts.Select(contact
+                => $"Name: {contact.Name}, Phone: {contact.PhoneNumber}"));
+
+            return;
+        }
+
+        Regex numRegex = new Regex(@"^[0-9]+$");
+        Regex alphaRegex = new Regex(@"^[a-zA-Z]+$");
+
+        List<Contact> results = new List<Contact>();
+
+        // 검색어에 영어만 쓰면 -> 이름으로 서치
+        // 검색어에 숫자만 쓰면 -> 전화번호로 서치
+        // 검색어에 섞어쓰면 -> 예외 처리
+        if (alphaRegex.IsMatch(searchQuery))
+        {
+            results = BinarySearchOnName(searchQuery);
+        }
+        else if (numRegex.IsMatch(searchQuery))
+        {
+            results = BinarySearchOnNumber(searchQuery);
+        }
+        else
+        {
+            searchResultText.text = "Input only numbers or only alphabets";
+            return;
+        }
+
+        // 결과가 비어있다면 -> 예외 처리
+        if (results.Any())
+        {
+            searchResultText.text = string.Join("\n", results.Select(contact
+                => $"Name: {contact.Name}, Phone: {contact.PhoneNumber}"));
+        }
+        else
+        {
+            searchResultText.text = "No contacts found";
+        }
+
+        /*
         IEnumerable<Contact> results;
 
         // 쿼리가 비어있다면 전체 출력
         if (searchQuery == "")
         {
-             results = contacts;
+            results = contacts;
         }
         else
         {
+            BinarySearchContact(searchQuery);
+
             results = contacts.Where(contact => contact.Name.Contains(searchQuery) || contact.PhoneNumber.Contains(searchQuery));
         }
 
@@ -109,6 +141,111 @@ public class Phonebook : MonoBehaviour
         {
             searchResultText.text = "No contacts found";
         }
+        */
+    }
+
+    private List<Contact> BinarySearchOnName(string searchQuery)
+    {
+        int left = 0;
+        int right = contacts.Count - 1;
+
+        // 오름차순 정렬
+        contacts = contacts.OrderBy(contact => contact.Name).ToList();
+
+        List<Contact> results = new List<Contact>();
+
+        while (left <= right)
+        {
+            int mid = (left + right) / 2;
+            var contact = contacts[mid];
+
+            // 검색어가 포함되면
+            if (contact.Name.ToLower().Contains(searchQuery))
+            {
+                results.Add(contact);
+
+                // 범위를 왼쪽으로 확장
+                int temp = mid - 1;
+                while (temp >= 0 && contacts[temp].Name.ToLower().Contains(searchQuery))
+                {
+                    results.Add(contacts[temp]);
+                    temp -= 1;
+                }
+
+                // 범위를 오른쪽으로 확장
+                temp = mid + 1;
+                while (temp < contacts.Count && contacts[temp].Name.ToLower().Contains(searchQuery))
+                {
+                    results.Add(contacts[temp]);
+                    temp += 1;
+                }
+
+                break;
+            }
+
+            if (string.Compare(contact.Name, searchQuery, true) < 0)
+            {
+                left = mid + 1;
+            }
+            else
+            {
+                right = mid - 1;
+            }
+        }
+
+        return results.OrderBy(contact => contact.Name).ToList();
+    }
+
+    private List<Contact> BinarySearchOnNumber(string searchQuery)
+    {
+        int left = 0;
+        int right = contacts.Count - 1;
+
+        // 오름차순 정렬
+        contacts = contacts.OrderBy(contact => contact.PhoneNumber).ToList();
+
+        List<Contact> results = new List<Contact>();
+
+        while (left <= right)
+        {
+            int mid = (left + right) / 2;
+            var contact = contacts[mid];
+
+            // 검색어가 포함되면
+            if (contact.PhoneNumber.Contains(searchQuery))
+            {
+                results.Add(contact);
+
+                // 범위를 왼쪽으로 확장
+                int temp = mid;
+                while (temp >= 0 && contacts[temp].PhoneNumber.Contains(searchQuery))
+                {
+                    results.Add(contacts[temp]);
+                    temp -= 1;
+                }
+
+                // 범위를 오른쪽으로 확장
+                temp = mid;
+                while (temp < contacts.Count && contacts[temp].PhoneNumber.Contains(searchQuery))
+                {
+                    results.Add(contacts[temp]);
+                    temp += 1;
+                }
+
+                break;
+            }
+
+            if (string.Compare(contact.PhoneNumber, searchQuery, true) < 0)
+            {
+                left = mid + 1;
+            }
+            else
+            {
+                right = mid - 1;
+            }
+        }
+
+        return results.OrderBy(contact => contact.Name).ToList();
     }
 
     public void AddContact()
